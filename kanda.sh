ce816de15e2be8c432b131de7feb3a940ea43f44
Kandagawa <goodgem11@gmail.com>
2026-01-15 09:53:30 +0700

    Update kanda.sh

--- kanda.sh
+++ kanda.sh
@@ -1,23 +1,26 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-# --- 1. GHIM LỆNH VÀO HỆ THỐNG (FIX LỖI KHÔNG NHẬN LỆNH) ---
+# Fix lỗi No such file .bashrc
+[ ! -f ~/.bashrc ] && touch ~/.bashrc
+
+# Tự ghim lệnh kanda vào hệ thống
 if [ ! -f "$PREFIX/bin/kanda" ]; then
-    # Tải nội dung từ GitHub và lưu trực tiếp thành file kanda
     curl -Ls is.gd/kandaprx -o $PREFIX/bin/kanda
     chmod +x $PREFIX/bin/kanda
-    # Cập nhật alias để gõ kanda là chạy file trong bin
-    grep -q "alias kanda" ~/.bashrc || echo "alias kanda='$PREFIX/bin/kanda'" >> ~/.bashrc
-    source ~/.bashrc
+    grep -q "alias kanda" ~/.bashrc || echo "alias kanda='kanda'" >> ~/.bashrc
 fi
 
-# --- 2. 100% MÃ NGUỒN GỐC ---
+# Dọn dẹp dữ liệu Tor cũ (Xóa sạch thay vì xóa app)
+pkill tor; pkill privoxy; sleep 1
+rm -rf $PREFIX/var/lib/tor/*
+
+# 100% MÃ NGUỒN GỐC CỦA NÍ
 pkg update -y && pkg install tor privoxy curl -y && \
 mkdir -p $PREFIX/etc/tor && \
 echo -e "StrictNodes 0\nMaxCircuitDirtiness 60\nCircuitBuildTimeout 5\nLog notice stdout" > $PREFIX/etc/tor/torrc && \
 sed -i 's/listen-address  127.0.0.1:8118/listen-address  0.0.0.0:8118/g' $PREFIX/etc/privoxy/config && \
 echo "forward-socks5t / 127.0.0.1:9050 ." >> $PREFIX/etc/privoxy/config && \
 grep -q "kanda.proxy" $PREFIX/etc/hosts || echo "127.0.0.1 kanda.proxy" >> $PREFIX/etc/hosts && \
-pkill tor; pkill privoxy; sleep 1; \
 clear && echo -e "--- ĐANG CHẠY FULL LOG (BẤM CTRL+C ĐỂ DỪNG) ---" && \
 privoxy --no-daemon $PREFIX/etc/privoxy/config & \
 tor
